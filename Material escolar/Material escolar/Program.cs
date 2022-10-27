f30285d62d27f244a2a01c48b4b03e120d43f46b
RosileneTi <rosa-clemente@hotmail.com>
2022-10-27 15:25:20 -0300

    termina Material

---
+++ Material escolar/Material escolar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Models.Caneta caneta = new Models.Caneta("Azul", "Bic", "Plastico");
+        caneta.Escrever("Lab 02");
+        caneta.Exibir();
+
+
+        Lapis lapis = new Lapis ()
+    }
+}
---
+++ Material escolar/Models/Caneta.cs
@@ -0,0 +1,96 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Models
+{
+    public class Canete
+    {
+        private string cor;
+        private string marca;
+        private  int  percentualCarga;
+        private string material;
+        private bool estourada;
+        private bool caida;
+        private bool tampada;
+
+            
+
+      public string Cor
+        {
+            get { return cor; } 
+            set { cor = value; }
+        }
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public int PercentualCarga
+        {
+            get {return percentualCarga; }
+            set { percentualCarga = value; }
+        }
+        public string Material
+        {
+            get { return material; }
+            set { material = value; }
+
+        }
+        public bool estourada
+        {
+            get { return estourada; }
+            set { estourada  = value}
+        }
+
+
+        public Canete(string)
+
+            cor;
+            marca;
+            percentualCarga;
+            material;
+            estourada;
+            caida;
+            tampada;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            public 
+
+
